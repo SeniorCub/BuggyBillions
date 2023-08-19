af719a5b7332f9ddffa6a930561ebb77601cfcf9
SeniorCub <farinderif@gmail.com>
2023-08-19 22:31:57 +0100

    I did a little practice on fetching APIs with Async method

--- Sholarship/Project/Login With Api/script.js
+++ Sholarship/Project/Login With Api/script.js
@@ -1,4 +1,4 @@
-let idd = window.prompt("Enter user id")
+
 const body = document.querySelector("body")
 const name = document.querySelector(".name")
 const username = document.querySelector(".username")
@@ -7,18 +7,37 @@ const email = document.querySelector(".email")
 const website = document.querySelector(".website")
 const address = document.querySelector(".address")
 
-if (idd <= 10 && idd >= 0) {
-     fetch("https://jsonplaceholder.typicode.com/users")
-.then(data => data.json())
-.then(data2 => {
-     name.innerHTML = data2[idd].name
-     username.innerHTML = `@${data2[idd].username}`
-     phone.innerHTML = `+${data2[idd].phone}`
-     email.innerHTML = `${data2[idd].email}`
-     website.innerHTML = `${data2[idd].website}`
-     address.innerHTML = `${data2[idd].address.street}, ${data2[idd].address.suite}, ${data2[idd].address.city}, ${data2[idd].address.zipcode}`
-})
-}else {
-     alert("You are not a user")
-     body.innerHTML = `<h1 style='text-align:center;'> This Id: ${idd} is not recognised</h1>`
+let active = document.querySelector('.active')
+active.addEventListener('click',go)
+
+let link = "https://jsonplaceholder.typicode.com/users"
+
+function go() {     
+     let idd = window.prompt("Enter user id")
+
+     if (idd <= 10 && idd >= 0) {
+          fetch(link)
+     .then(data => data.json())
+     .then(data2 => {
+          name.innerHTML = data2[idd].name
+          username.innerHTML = `@${data2[idd].username}`
+          phone.innerHTML = `+${data2[idd].phone}`
+          email.innerHTML = `${data2[idd].email}`
+          website.innerHTML = `${data2[idd].website}`
+          address.innerHTML = `${data2[idd].address.street}, ${data2[idd].address.suite}, ${data2[idd].address.city}, ${data2[idd].address.zipcode}`
+     })
+     }else {
+          alert("You are not a user")
+          body.innerHTML = `<h1 style='text-align:center;'> This Id: ${idd} is not recognised</h1>`
+     }
+};
+
+
+let get = async() => {
+     let getting = await fetch(link)
+     let gett = await getting.json()
+     gett.forEach(gett => {
+          console.log(gett.address)
+     });
 }
+get()
