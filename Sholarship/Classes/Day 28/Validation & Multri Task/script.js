227ce929a84143ea61e7f19b19aa98454e2b59c3
SeniorCub <farinderif@gmail.com>
2023-08-23 17:36:47 +0100

    Trying to send code during class

--- Sholarship/Classes/Day 28/Validation & Multri Task/script.js
+++ Sholarship/Classes/Day 28/Validation & Multri Task/script.js
@@ -1,76 +1,114 @@
-// const nexts = document.querySelector('.next1').addEventListener('click', next);
-// const form1 = document.querySelector('.form1');
-// const form2 = document.querySelector('.form2');
-// const form3 = document.querySelector('.form3');
+// Forms
+const form1 = document.querySelector('.form1');
+const form2 = document.querySelector('.form2');
+const form3 = document.querySelector('.form3');
 
-// function next() {
-//      // alert('Hello')
+// Step 1
+const next1 = document.querySelector('.next1').addEventListener('click', next1);
+function next1() {
+     form1.style.left = '-100px';
+     form2.style.left = '550px';
+};
+
+// // Step 2
+// const next2 = document.querySelector('.next1').addEventListener('click', next2);
+// const back2 = document.querySelector('.back1').addEventListener('click', back2);
+// function next2() {
 //      form1.style.left = '-200px'
-//      form2.style.left = '100px' 
-// }
+//      form2.style.left = '100px'
+// };
+// function back2() {
+//      form1.style.right = '-200px'
+//      form2.style.right = '100px'
+// };
 
-// // NOTES
-//      // to add and remove class
-//      // form1.classList.add('form')
-//      // form1.classList.remove('form')
+// // Step 3
+// const next3 = document.querySelector('.next1').addEventListener('click', next3);
+// const back3 = document.querySelector('.back1').addEventListener('click', back3);
+// function next3() {
+//      form1.style.left = '-200px'
+//      form2.style.left = '100px'
+// };
+// function back3() {
+//      form1.style.right = '-200px'
+//      form2.style.right = '100px'
+// };
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 // JavaScript code from Chat GPT
-document.addEventListener("DOMContentLoaded", function() {
-     const forms = document.querySelectorAll(".forms form");
-     const steps = document.querySelectorAll(".steps div");
-     let currentStep = 0;
+// document.addEventListener("DOMContentLoaded", function() {
+//      const forms = document.querySelectorAll(".forms form");
+//      const steps = document.querySelectorAll(".steps div");
+//      let currentStep = 0;
  
-     function showStep(step) {
-         forms.forEach((form, index) => {
-             if (index === step) {
-                 form.style.left = `${100 * index}px`;
-             } else {
-                 form.style.left = "100%";
-             }
-         });
+//      function showStep(step) {
+//          forms.forEach((form, index) => {
+//              if (index === step) {
+//                  form.style.left = `${100 * index}px`;
+//              } else {
+//                  form.style.left = "100%";
+//              }
+//          });
  
-         steps.forEach((stepElement, index) => {
-             if (index === step) {
-                 stepElement.classList.add("active");
-             } else {
-                 stepElement.classList.remove("active");
-             }
-         });
-     }
+//          steps.forEach((stepElement, index) => {
+//              if (index === step) {
+//                  stepElement.classList.add("active");
+//              } else {
+//                  stepElement.classList.remove("active");
+//              }
+//          });
+//      }
  
-     function nextStep() {
-         if (currentStep < forms.length - 1) {
-             currentStep++;
-             showStep(currentStep);
-         }
-     }
+//      function nextStep() {
+//          if (currentStep < forms.length - 1) {
+//              currentStep++;
+//              showStep(currentStep);
+//          }
+//      }
  
-     function previousStep() {
-         if (currentStep > 0) {
-             currentStep--;
-             showStep(currentStep);
-         }
-     }
+//      function previousStep() {
+//          if (currentStep > 0) {
+//              currentStep--;
+//              showStep(currentStep);
+//          }
+//      }
  
-     showStep(currentStep);
+//      showStep(currentStep);
  
-     const nextButtons = document.querySelectorAll(".next");
-     const backButtons = document.querySelectorAll(".back");
+//      const nextButtons = document.querySelectorAll(".next");
+//      const backButtons = document.querySelectorAll(".back");
  
-     nextButtons.forEach((button, index) => {
-         button.addEventListener("click", function(event) {
-             event.preventDefault();
-             nextStep();
-         });
-     });
+//      nextButtons.forEach((button, index) => {
+//          button.addEventListener("click", function(event) {
+//              event.preventDefault();
+//              nextStep();
+//          });
+//      });
  
-     backButtons.forEach((button, index) => {
-         button.addEventListener("click", function(event) {
-             event.preventDefault();
-             previousStep();
-         });
-     });
- });
+//      backButtons.forEach((button, index) => {
+//          button.addEventListener("click", function(event) {
+//              event.preventDefault();
+//              previousStep();
+//          });
+//      });
+//  });
+
+
+
+// NOTES
+     // to add and remove class
+     // form1.classList.add('form')
+     // form1.classList.remove('form')
