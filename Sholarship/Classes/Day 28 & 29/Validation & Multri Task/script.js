b5ee6d15c9e1aafb7cb163fe90d985216e75a4b5
SeniorCub <farinderif@gmail.com>
2023-08-23 18:26:47 +0100

    Finished the multi tasking JavaScript for scrolling

--- Sholarship/Classes/Day 28 & 29/Validation & Multri Task/script.js
+++ Sholarship/Classes/Day 28 & 29/Validation & Multri Task/script.js
@@ -1,41 +1,52 @@
-// Forms
-const form1 = document.querySelector('.form1');
-const form2 = document.querySelector('.form2');
-const form3 = document.querySelector('.form3');
+// Next on Form one
+const next1 = document.querySelector('.next1').addEventListener("click", btn_next1);
+// Next and Back on Form 2
+const next2 = document.querySelector('.next2').addEventListener("click", btn_next2);
+const back2 = document.querySelector('.back2').addEventListener("click", btn_back2);
+// Next and Back on Form 3
+// const next3 = document.querySelector('.next3').addEventListener("click", btn_next3);
+const back3 = document.querySelector('.back3').addEventListener("click", btn_back3);
+
 
 // Step 1
-const next1 = document.querySelector('.next1').addEventListener('click', next1);
-function next1() {
-     form1.style.left = '-100px';
-     form2.style.left = '550px';
+function btn_next1() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+
+     form1.style.left = '-600px'
+     form2.style.left = '130px'
+     form3.style.left = '600px'
 };
+// Step 2
+function btn_next2() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
 
-// // Step 2
-// const next2 = document.querySelector('.next1').addEventListener('click', next2);
-// const back2 = document.querySelector('.back1').addEventListener('click', back2);
-// function next2() {
-//      form1.style.left = '-200px'
-//      form2.style.left = '100px'
-// };
-// function back2() {
-//      form1.style.right = '-200px'
-//      form2.style.right = '100px'
-// };
+     form1.style.left = '-1070px'
+     form2.style.left = '-600px'
+     form3.style.left = '130px'
+};
+function btn_back2() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
 
-// // Step 3
-// const next3 = document.querySelector('.next1').addEventListener('click', next3);
-// const back3 = document.querySelector('.back1').addEventListener('click', back3);
-// function next3() {
-//      form1.style.left = '-200px'
-//      form2.style.left = '100px'
-// };
-// function back3() {
-//      form1.style.right = '-200px'
-//      form2.style.right = '100px'
-// };
+     form1.style.left = '130px'
+     form2.style.left = '600px'
+     form3.style.left = '1070px'
+};
+// Step 3
+function btn_back3() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
 
-
-
+     form1.style.left = '-600px'
+     form2.style.left = '130px'
+     form3.style.left = '600px'
+};
 
 
 
