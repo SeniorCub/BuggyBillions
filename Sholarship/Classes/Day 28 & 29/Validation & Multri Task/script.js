585de6f53c9834a6b9be75285f40ab68182b98ea
SeniorCub <farinderif@gmail.com>
2023-08-24 16:38:45 +0100

    Created an additional form 4 to confirm if the user is a robot, using just a regular checkbox input

--- Sholarship/Classes/Day 28 & 29/Validation & Multri Task/script.js
+++ Sholarship/Classes/Day 28 & 29/Validation & Multri Task/script.js
@@ -4,8 +4,10 @@ const next1 = document.querySelector('.next1').addEventListener("click", btn_nex
 const next2 = document.querySelector('.next2').addEventListener("click", btn_next2);
 const back2 = document.querySelector('.back2').addEventListener("click", btn_back2);
 // Next and Back on Form 3
-// const next3 = document.querySelector('.next3').addEventListener("click", btn_next3);
+const next3 = document.querySelector('.next3').addEventListener("click", btn_next3);
 const back3 = document.querySelector('.back3').addEventListener("click", btn_back3);
+// Back and Next on Form 4
+const back4 = document.querySelector('.back4').addEventListener("click", btn_back4);
 
 
 // Step 1
@@ -13,49 +15,83 @@ function btn_next1() {
      const form1 = document.querySelector('.form1')
      const form2 = document.querySelector('.form2')
      const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
      const line2 =document.querySelector('.line2')
 
      line2.style.display = 'block'
      form1.style.left = '-600px'
      form2.style.left = '130px'
      form3.style.left = '600px'
+     form4.style.left = '1070px'
 };
 // Step 2
 function btn_next2() {
      const form1 = document.querySelector('.form1')
      const form2 = document.querySelector('.form2')
      const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
      const line3 =document.querySelector('.line3')
 
      line3.style.display = 'block'
      form1.style.left = '-1070px'
      form2.style.left = '-600px'
      form3.style.left = '130px'
+     form4.style.left = '600px'
 };
 function btn_back2() {
      const form1 = document.querySelector('.form1')
      const form2 = document.querySelector('.form2')
      const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
      const line2 =document.querySelector('.line2')
 
      line2.style.display = 'none'
      form1.style.left = '130px'
      form2.style.left = '600px'
      form3.style.left = '1070px'
+     form4.style.left = '1540px'
 };
 // Step 3
 function btn_back3() {
      const form1 = document.querySelector('.form1')
      const form2 = document.querySelector('.form2')
      const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
      const line3 =document.querySelector('.line3')
 
      line3.style.display = 'none'
      form1.style.left = '-600px'
      form2.style.left = '130px' 
      form3.style.left = '600px'
+     form4.style.left = '1070px'
 };
+function btn_next3() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line4 =document.querySelector('.line4')
 
+     line4.style.display = 'block'
+     form1.style.left = '-1540px'
+     form2.style.left = '-1070px' 
+     form3.style.left = '-600px'
+     form4.style.left = '130px'
+};
+// Step 4
+function btn_back4() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line4 =document.querySelector('.line4')
+
+     line4.style.display = 'none'
+     form1.style.left = '-1070px'
+     form2.style.left = '-600px'
+     form3.style.left = '130px'
+     form4.style.left = '600px'
+};
 
 
 
