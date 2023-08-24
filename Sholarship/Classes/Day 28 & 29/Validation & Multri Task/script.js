c973ce577ab1dfd79565919b430f5de98326b174
SeniorCub <farinderif@gmail.com>
2023-08-24 18:21:35 +0100

    Finished Validation that does not allow empty inputs and same password

--- Sholarship/Classes/Day 28 & 29/Validation & Multri Task/script.js
+++ Sholarship/Classes/Day 28 & 29/Validation & Multri Task/script.js
@@ -9,6 +9,20 @@ const back3 = document.querySelector('.back3').addEventListener("click", btn_bac
 // Back and Next on Form 4
 const back4 = document.querySelector('.back4').addEventListener("click", btn_back4);
 
+// Inputs on Form 1
+let firstName = document.querySelector('#first_name');
+let lastName = document.querySelector('#last_name');
+let userName = document.querySelector('#user_name');
+// Inputs on Form 2
+let email = document.querySelector('#email');
+let address = document.querySelector('#address');
+let age = document.querySelector('#age');
+// Inputs on Form 3
+let crPwd = document.querySelector('#CrPwd');
+let coPwd = document.querySelector('#CoPwd');
+
+// For Input filter
+const fullNamePattern = /^[A-Za-z\s'\-]+$/;
 
 // Step 1
 function btn_next1() {
@@ -17,12 +31,16 @@ function btn_next1() {
      const form3 = document.querySelector('.form3')
      const form4 = document.querySelector('.form4')
      const line2 =document.querySelector('.line2')
-
-     line2.style.display = 'block'
-     form1.style.left = '-600px'
-     form2.style.left = '130px'
-     form3.style.left = '600px'
-     form4.style.left = '1070px'
+     
+     if (firstName.value == "" || lastName.value == "" || userName.value == "") {
+          alert('Error')
+     }else {
+          line2.style.display = 'block'
+          form1.style.left = '-600px'
+          form2.style.left = '130px'
+          form3.style.left = '600px'
+          form4.style.left = '1070px'
+     }
 };
 // Step 2
 function btn_next2() {
@@ -32,11 +50,15 @@ function btn_next2() {
      const form4 = document.querySelector('.form4')
      const line3 =document.querySelector('.line3')
 
+     if (email.value == "" || address.value == "" || age.value == "") {
+          alert('Error')
+     }else {
      line3.style.display = 'block'
      form1.style.left = '-1070px'
      form2.style.left = '-600px'
      form3.style.left = '130px'
      form4.style.left = '600px'
+     }
 };
 function btn_back2() {
      const form1 = document.querySelector('.form1')
@@ -71,12 +93,19 @@ function btn_next3() {
      const form3 = document.querySelector('.form3')
      const form4 = document.querySelector('.form4')
      const line4 =document.querySelector('.line4')
-
-     line4.style.display = 'block'
-     form1.style.left = '-1540px'
-     form2.style.left = '-1070px' 
-     form3.style.left = '-600px'
-     form4.style.left = '130px'
+     if (crPwd.value == "" || coPwd.value == "") {
+               alert('Error')
+          }
+          else {
+                if (crPwd.value == coPwd) {
+               line4.style.display = 'block'
+               form1.style.left = '-1540px'
+               form2.style.left = '-1070px' 
+               form3.style.left = '-600px'
+               form4.style.left = '130px'
+                }else {
+               alert('Passord is not the same')
+          }}
 };
 // Step 4
 function btn_back4() {
