e8e15fc817b8f1b29f54bc6cc991b32b61a31a44
SeniorCub <farinderif@gmail.com>
2023-08-26 15:40:00 +0100

    I just discovered i spelt Scholarship wrongly

---
+++ Scholarship/Classes/Day31/math.js
@@ -0,0 +1,14 @@
+const add = function (a,b) {
+     return a+b;
+};
+const subtract = function (c,d) {
+     return c-d;
+};
+const multiply = function (e,f) {
+     return e*f;
+};
+const division = function (g,h) {
+     return g/h;
+};
+
+module.exports = add
---
+++ Scholarship/Classes/Day31/script.js
@@ -0,0 +1,5 @@
+const add = require("./math")
+
+console.log("Hello, I am Here");
+
+console.log(add(3,2));
