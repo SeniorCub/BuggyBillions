904654577dad85bbd5fce5a718a3f8bab615838c
SeniorCub <farinderif@gmail.com>
2023-08-27 18:23:57 +0100

    Started a personal Project, a ToDo List for the start

---
+++ Scholarship/Project/ToDoList/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".btn").addEventListener('click', () => {
+     const list = document.querySelector("#myList")
+     const out = document.querySelector(".out")
+     out.innerHTML = 'list'
+})
