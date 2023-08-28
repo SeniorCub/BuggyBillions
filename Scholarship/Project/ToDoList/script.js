eef630a9e518fd4ede6688c2399a5c5e95580cbd
SeniorCub <farinderif@gmail.com>
2023-08-28 23:03:26 +0100

    Did assignment, class work and lot more

--- Scholarship/Project/ToDoList/script.js
+++ Scholarship/Project/ToDoList/script.js
@@ -1,5 +1,12 @@
 const btn = document.querySelector(".btn").addEventListener('click', () => {
      const list = document.querySelector("#myList")
      const out = document.querySelector(".out")
-     out.innerHTML = 'list'
+     var listt = ""
+         listt += '<div>'
+         listt += list.value
+         listt += '</div>'
+         listt += '<button>'
+         listt += 'Remove'
+         listt += '</button>'
+     out.innerHTML = listt;
 })
