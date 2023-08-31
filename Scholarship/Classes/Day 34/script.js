f4b4911c44e29a79733ff6cbbade8e2336da90f3
SeniorCub <farinderif@gmail.com>
2023-08-31 17:42:23 +0100

    Done with today's class

--- Scholarship/Classes/Day 34/script.js
+++ Scholarship/Classes/Day 34/script.js
@@ -1,8 +1,15 @@
+// GEt Button
 let getBtn = document.querySelector('.get')
 let result = document.querySelector('.result')
 const getBtnClick = async() => {
      try {
-          const get = await axios('https://fakestoreapi.com/products')
+          const get = await axios({
+               method: "get",
+               url: "https://jsonplaceholder.typicode.com/users/",
+               params : {
+                    _limit : 5
+               }
+          })
           let str = JSON.stringify(get)
           console.log(str)
           console.log(JSON.parse(str))
@@ -12,4 +19,29 @@ const getBtnClick = async() => {
           console.error(error)
      }
 }
+
+// Post Button
+let postBtn = document.querySelector('.post')
+const postBtnClick = async() => {
+     try {
+          const post = await axios({
+               method: "post",
+               url: "https://jsonplaceholder.typicode.com/users/",
+               data: {
+                    name: "Reuben Farinde",
+                    username: "SeniorCub",
+                    email: "farinderif@gmail.com"
+               }
+          })
+          let pos = JSON.stringify(post.data)
+          console.log(pos)
+          console.log(JSON.parse(pos))
+
+          result.textContent = pos
+     } catch (error) {
+          console.error(error)
+     }
+}
+
 getBtn.addEventListener("click", getBtnClick)
+postBtn.addEventListener("click", postBtnClick)
