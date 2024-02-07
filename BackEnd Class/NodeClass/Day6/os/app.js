bb0fedec7d2fa787f439e2f081a9aae4bbd4cee1
seniorcub <farinderif@gmail.com>
2024-02-07 10:27:08 +0100

    Learnt about OS as a middleware

---
+++ BackEnd Class/NodeClass/Day6/os/app.js
@@ -0,0 +1,9 @@
+const os = require("os")
+
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.cpus());
+console.log(os.version());
+console.log(os.type());
