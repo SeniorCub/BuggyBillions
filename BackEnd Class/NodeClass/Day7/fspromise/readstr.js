bcb3f68d78495f0ca5a076d6cf08eba08bd73c2f
seniorcub <farinderif@gmail.com>
2024-02-08 11:02:55 +0100

    Learnt about Read Stream as a middleware

--- BackEnd Class/NodeClass/Day7/fspromise/readstr.js
+++ BackEnd Class/NodeClass/Day7/fspromise/readstr.js
@@ -1,2 +1,7 @@
 let fs = require("fs");
+const path = require("path")
 
+const readStr = fs.createReadStream(path.join(__dirname,"texts","forth.txt"));
+const writeStr = fs.createWriteStream(path.join(__dirname,"texts","fifth.txt"));
+
+readStr.pipe(writeStr);
