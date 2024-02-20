99d163817dbae8b2cafb8f59ac07d23301d46e69
seniorcub <farinderif@gmail.com>
2024-02-20 14:25:14 +0100

    done with class for today

--- BackEnd Class/NodeClass/Day9/EXPRESS/server.js
+++ BackEnd Class/NodeClass/Day9/EXPRESS/server.js
@@ -1,8 +1,47 @@
-let http = require('http')
-let path = require('path')
-let fs = require('fs')
+// let http = require('http')
+// let path = require('path')
+// let fs = require('fs')
+
+import http from 'http'
+import path from 'path'
+import fs from 'fs'
 
 const server = http.createServer( (req, res) => {
-
+     if (req.url == "/") {
+          fs.readFile(path.join("/home/reuben/BuggyBillions/BackEnd Class/NodeClass/Day9/EXPRESS", "CryptoSite", "index.html"), "utf-8", (err, data) => {
+               if (err) {
+                    res.writeHead(404, {"Content-Type": "text/plain"})
+                    res.end(err)
+               } else {
+                    res.writeHead(200, {"Content-Type":"text/html"})
+                    res.end(data)
+               }
+          })
+     }
+     if (req.url == "/style.css") { 
+          fs.readFile(path.join("/home/reuben/BuggyBillions/BackEnd Class/NodeClass/Day9/EXPRESS", "CryptoSite", "style.css"), "utf-8", (err, data) => {
+               if (err) {
+                    res.writeHead(404, {"Content-Type": "text/plain"})
+                    res.end(err)
+               } else {
+                    res.writeHead(200, {"Content-Type":"text/css"})
+                    res.end(data)
+               }
+          })
+     }
+     if (req.url == "/script.js") { 
+          fs.readFile(path.join("/home/reuben/BuggyBillions/BackEnd Class/NodeClass/Day9/EXPRESS/CryptoSite", "script.js"), "utf-8", (err, data) => {
+               if (err) {
+                    res.writeHead(404, {"Content-Type": "text/plain"})
+                    res.end(err)
+               } else {
+                    res.writeHead(200, {"Content-Type":"text/script"})
+                    res.end(data)
+               }
+          })
+     }
 });
-server.listen(8080)
+let port = 8080
+server.listen(port, () => {
+     console.log(`Server running on port ${port}`);
+})
