7dfa1635eb6194c1e6c65645d1d7beb6e1b56c28
seniorcub <farinderif@gmail.com>
2024-02-24 01:31:44 +0100

    Update server.js and package.json

---
+++ BackEnd Class/NodeClass/Day11/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import path from 'path';
+import bcrypt from 'bcrypt';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(process.cwd(), "/public")))
+
+
+app.post('/signUp.html', (req, res) => {
+     try {
+         const {first, last, email, password} = req.body
+         bcrypt.hash(password, 10, (err, hash) => {
+             if (err) throw err;
+             console.log(
+                 {
+                     "firstname": first,
+                     "lastname": last,
+                     "email": email,
+                     "password": hash,
+                 }
+             )
+         })
+         res.sendFile(path.join(process.cwd(), "public", "index.html"));
+     } catch (error) {
+         res.writeHead(404);
+         res.end(error);
+     }
+ });
+ 
+ app.get('/signUp.html', (req, res) =>{
+     res.sendFile(path.join(process.cwd(), "public", "signUp.html"))
+ });
+
+const port = 5050
+app.listen(port, ()=>{
+     console.log(`Server running on http://localhost:${port}`);
+})
