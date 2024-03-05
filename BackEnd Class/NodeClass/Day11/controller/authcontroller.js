b1f6da5da498344dd9e5f7915f62e6dd42e2cd11
seniorcub <farinderif@gmail.com>
2024-03-05 13:08:02 +0100

    Done with todays class. Learnt about  JWT authentication and login functionality

--- BackEnd Class/NodeClass/Day11/controller/authcontroller.js
+++ BackEnd Class/NodeClass/Day11/controller/authcontroller.js
@@ -1,6 +1,9 @@
-import { passwordHash } from "../helper/authhelper.js";
+import { use } from "bcrypt/promises.js";
+import { passwordHash , passwordCompare} from "../helper/authhelper.js";
 import usermodels from "../models/usermodels.js";
+import jwt from "jsonwebtoken";
 
+// For Register
 export const registerClient = async (req, res) => {
      try {
          const {name, userName, email, password} = req.body
@@ -33,7 +36,50 @@ export const registerClient = async (req, res) => {
                newUser
           })
      } catch (error) {
-         res.writeHead(404);
+         res.status(404);
          res.end(error);
      }
- }
+}
+
+// For Login
+export const loginClient = async (req, res) => {
+     try {
+          const{email, password}= req.body;
+          if (!email || !password) {
+               return res.status(201).send({
+                    success: false,
+                    msg: "Unable to Login, please enter your email and password"
+               })
+          }
+          const user = await usermodels.findOne({email});
+          if (!user) {
+               return res.status(404).send({
+                    success: false,
+                    msg: "Invalid Email, User does not exist"
+               })
+          }
+
+          const match = await passwordCompare(password, user.password);
+          if (!match) {
+               return res.status(201).send({
+                    success: false,
+                    msg: "Invalid Password, please enter a valid password"
+               })
+          }
+          const token = await jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '2d'});
+          res.status(200).send({
+               success: true,
+               msg: "Login successful!!",
+               user: {
+                    name: user.name,
+                    email: user.email,
+                    userName: user.userName
+
+               },
+               token
+          })
+     } catch (error) {
+          res.status(404);
+          res.end(error);
+     }
+}
--- BackEnd Class/NodeClass/Day11/routes/authroute.js
+++ BackEnd Class/NodeClass/Day11/routes/authroute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerClient } from '../controller/authcontroller.js';
+import { registerClient, loginClient } from '../controller/authcontroller.js';
 const router = express.Router();
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
 router.post('/signUp', registerClient);
+router.post('/login', loginClient);
 export default router;
